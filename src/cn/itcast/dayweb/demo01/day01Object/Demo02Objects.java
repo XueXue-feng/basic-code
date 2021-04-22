60d6efe771c8030956cf7fa9ed94b567c92e904f
= <1614663679@qq.com>
2021-04-22 23:06:09 +0800

    2021/04/22 23:05 Date,StringBuilder,System的练习

---
+++ src/cn/itcast/dayweb/demo01/day01Object/Demo02Objects.java
@@ -0,0 +1,23 @@
+package cn.itcast.dayweb.demo01.day01Object;
+
+import java.util.Objects;
+
+public class Demo02Objects {
+    public static void main(String[] args) {
+        String str1 = "abs";
+        String str2 = "abs";
+        //String 重写了equals方法比较的是属性
+        System.out.println(str1.equals(str2));//true
+
+        //如果str1为null则会报空指针异常,Objects这个类的equals方法可以解决
+        /*
+        容忍空指针异常
+        * public static boolean equals(Object a, Object b) {
+            return (a == b) || (a != null && a.equals(b));
+        }
+        * */
+        String str3 = null;
+        System.out.println(Objects.equals(str3,str1));//false
+
+    }
+}
---
+++ src/cn/itcast/dayweb/demo01/demo04StringBuffer/Demo01StringBuffer.java
@@ -0,0 +1,9 @@
+package cn.itcast.dayweb.demo01.demo04StringBuffer;
+
+/*
+* String类底层是一个被final修饰的常量，进行字符串的相加内存中就会产生多个字符串，
+* 占用空间效率低下。
+* StringBuilder:底层是一个数组但是没有被final修饰(看成是一个长度可以变化的数组),字符串缓冲区byte[] value长度为16
+* */
+public class Demo01StringBuffer {
+}
