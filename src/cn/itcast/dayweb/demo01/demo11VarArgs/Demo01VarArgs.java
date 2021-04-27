9d89f4d39f402a0d7e0233dabc3311d44ee1e420
= <1614663679@qq.com>
2021-04-27 11:38:24 +0800

    2021/04/27 11:37 Map 的练习

---
+++ src/cn/itcast/dayweb/demo01/demo11VarArgs/Demo01VarArgs.java
@@ -0,0 +1,36 @@
+package cn.itcast.dayweb.demo01.demo11VarArgs;
+
+/*
+* 当方法的参数的数据类型一样但是个数不确定的时候就可以使用JDK1.5+
+* 格式：修饰符 返回值类型 方法名(参数类型... 变量名){}
+* 原理：底层就是一个数组根据传递的参数个数不同会创建不同类型的数组来存储这些参数，存储的个数可以是0,1,2...
+* 注意：1.一个方法的可变参数只能有一个 2.如果方法有多个参数，可变参数必须写在末尾
+* */
+public class Demo01VarArgs {
+    public static void main(String[] args) {
+      //System.out.println(sumN(1,2,3,4,5));//15
+        System.out.println(sumA(100,1,2,3,4,5,6,7,8,9));//145
+    }
+
+    //定义计算0~n个的和
+    public static int sumN(int...array){
+        System.out.println(array);//底层是一个长度
+        int sum = 0;
+        for(int a:array){
+            sum = sum + a;
+        }
+        return sum;
+    }
+    //定义一个方法计算两个int类型的和
+    public static int sun(int a,int b){
+        return  a + b;
+    }
+    //定义某几个int类型的数据与int a的和
+    public static int sumA(int a,int...array){
+        int sum = 0;
+        for(int i:array){
+            sum = sum  + i;
+        }
+        return sum + a;
+    }
+}
---
+++ src/cn/itcast/dayweb/demo01/demo13Map/Demo01Map.java
@@ -0,0 +1,46 @@
+package cn.itcast.dayweb.demo01.demo13Map;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/*
+* java.util.Map<K,V>集合特点：1.是一个双列几个，一个元素包含两个元素，K键，V值
+* 2.Key和Value中的数据类型可以相同也可以不同
+* 3.Map中的K不重复V可以重复
+* 4.K与V一一对应
+* java.util.HashMap<K,V>特点：1.hashmap底层是哈希表，查询速度快，JDK1.7之前是数组+链表
+* JDK1.8+是数组+单向链表或者是数组+红黑树（链表的长度>=8）
+* 2.是一个无序几个
+* java.util.LinkedHashMap<K,V> extends HshMap<K,V>1.是一个有序的集合
+* 2.底层是哈希表+链表的结构
+* */
+public class Demo01Map {
+    public static void main(String[] args) {
+        //Map对象
+        Map<Integer,String> map = new HashMap<>();
+        //put(K key,V value)方法向map中添加数据,如果K唯一则返回Null,如果K不唯一则返回倍替代的值。
+        map.put(1,"迪丽热巴");
+        map.put(2,"古力娜扎");
+        map.put(3,"马尔扎哈");
+        map.put(4,"杨幂");
+        System.out.println(map);//{1=迪丽热巴, 2=古力娜扎, 3=马尔扎哈, 4=杨幂}重写了toString()
+
+        //get(K key)
+        System.out.println(map.get(4));
+
+        //remove()方法
+        System.out.println(map.remove(3));
+        System.out.println(map.get(3));//null
+
+        //containKey()
+        System.out.println(map.containsKey(2));//true
+
+        //遍历map
+        Set<Integer> set = map.keySet();
+        for (Integer integer : set) {
+            System.out.println(map.get(integer));
+        }
+
+    }
+}
